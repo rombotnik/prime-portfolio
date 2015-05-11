b74fc8a926741806633e92c4e5365d64485413af
Rom Stevens <romney.stevens@gmail.com>
2015-05-10 20:58:39 -0500

    Create setup() and displayStories() in client

--- client/app.js
+++ client/app.js
@@ -1,14 +1,35 @@
-var i, j;
+var i, j, storyHTML, storyData;
 var colors = ["#f44336", "#E91E63", "#9C27B0", "#9C27B0", "#3F51B5", "#2196F3", "#03A9F4", "#00BCD4", "#009688", "#4CAF50", "#8BC348", "#CDDC39", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722"];
 var colorIndex = 1;
 
-$(document).ready(function(){
-    $.get('/writing', function(data){
-        console.log(data);
-        $.get('/writing/' + data.writing[0].file, function(data){
-            console.log(data);
-        });
+function setup() {
+    // First, get the story template and writing JSON data
+    // Then, once both are present, call displayStories()
+    $.get('views/story.html', function(template){
+        storyHTML = template;
+        if (storyData) {
+            displayStories();
+        }
     });
+    $.get('/writing', function(data){
+        storyData = data.writing;
+        if (storyHTML) {
+            displayStories();
+        }
+    });
+}
+
+function displayStories() {
+    for (i=0; i<storyData.length; i++) {
+        $.get('/writing/' + storyData[i].file, function(text){
+            console.log(text);
+        });
+    }
+}
+
+$(document).ready(function(){
+
+    setup();
 
     // Makes rainbows out of the headers
     setInterval(function(){
